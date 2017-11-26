66bc5502aeecf8d0aa16b3811631aa11f29373e9
Wenzel Massag <mail@wmassag.de>
2017-11-26 17:21:13 +0100

    Added classes
    - Health
    - UserInputAbstraction

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public bool regenerateWhileFightingWithShield = true;
+    public bool regenerateWhileFightingNoShield = false;
+    public float autoRegenerateDelayInSeconds = 0.5f;
+    public Transform destroyEffect;
+    private float healthStatus = 1f;
+
+    public float getHealth(){
+        return healthStatus;
+    }
+    private IEnumerator selfDestructAtEndOfFrame(){
+        yield return new WaitForEndOfFrame();
+        Destroy(this.gameObject);
+    }
+    public float changeHealth(float difference, bool autoDestroy = true){
+        healthStatus += difference;
+        healthStatus = Mathf.Clamp(shieldStatus, 0f, 1f);
+        if(autoDestroy && destroyEffect != null){
+            Instantiate(destroyEffect, this.transform.position, this.transform.rotation);
+            StartCoroutine(selfDestructAtEndOfFrame());
+        }
+        return getHealth();
+    }
+
+
+    public bool hasShield = false;
+    public Transform shieldTransform;
+    private float shieldStatus = 1f;
+    private Transform shieldInstance;
+
+    public float getShieldStatus(){
+        return (hasShield) ? shieldStatus : 0f;
+    }
+    private void disableShield(){
+        hasShield = false;
+        shieldStatus = 1f;
+        if(shieldInstance != null){
+            Destroy(shieldInstance.gameObject);
+            shieldInstance = null;
+        }
+    }
+    private void enableShield(){
+        hasShield = true;
+        shieldStatus = 1f;
+        if(shieldInstance == null){
+            shieldInstance = Instantiate(shieldTransform, this.transform.position, this.transform.rotation, this.transform);
+        }
+    }
+    public float changeShieldStatus(bool b){
+        if(b){
+            enableShield();
+        }else{
+            disableShield();
+        }
+        return getShieldStatus();
+    }
+    public float changeShieldStatus(float difference){
+        shieldStatus += difference;
+        shieldStatus = Mathf.Clamp(shieldStatus, 0f, 1f);
+        if (shieldStatus.Equals(0f)){
+            disableShield();
+        }
+        return getShieldStatus();
+    }
+}
---
+++ Assets/Scripts/UserInputAbstraction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputAbstraction : MonoBehaviour
+{
+    public static Vector2 userMotionVector()
+    {
+        Vector2 motion = new Vector2();
+#if UNITY_EDITOR || UNITY_STANDALONE
+        motion.x = Input.GetAxis("Horizontal");
+        motion.y = Input.GetAxis("Vertical");
+#endif
+
+#if UNITY_IOS
+#endif
+        return motion;
+    }
+    public static Vector3 usertargetCoordinateWorldSpace(LayerMask mask){
+
+        return new Vector3();
+    }
+}
