4a45c042c5e99a14f62f7cdb856a4286863f01fa
Wenzel Massag <mail@wmassag.de>
2017-11-27 19:14:23 +0100

    Player Motion update

--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -5,18 +5,44 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour {
 
     public float moveSpeed = 1;
+    private float backupInitialDragValue;
+    private Rigidbody me;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    void Start(){
+        backupInitialDragValue = GetComponent<Rigidbody>().drag;
+        me = GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        shoot();
+        setDragDefault();
         move();
 	}
 
+    private void setDragDefault(){
+        me.drag = backupInitialDragValue;
+    }
+
+    public void receiveForce(Vector3 force3D, float dragDifference = 0f){
+        force3D.y = 0f;
+        if (!force3D.magnitude.Equals(0f)){
+            me.AddForce(force3D, ForceMode.Force);
+        }
+        if (!dragDifference.Equals(0f)){
+            me.drag = backupInitialDragValue + dragDifference;
+        }
+    }
+
     private void move(){
-        GetComponent<Rigidbody>().AddForce(UserInputAbstraction.userMotionVector() * moveSpeed);
+        me.AddForce(UserInputAbstraction.motionVector() * moveSpeed * Time.deltaTime, ForceMode.Force);
+    }
+
+    private void shoot(){
+        if(UserInputAbstraction.isShooting()){
+            //GetComponent -> Weapon -> isFiring=true
+        }else{
+            //GetComponent -> Weapon -> isFiring=false
+        }
     }
 }
--- Assets/Scripts/UserInputAbstraction.cs
+++ Assets/Scripts/UserInputAbstraction.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class UserInputAbstraction : MonoBehaviour
 {
-    public static Vector3 userMotionVector()
+    public static Vector3 motionVector()
     {
         Vector3 motion = new Vector3();
 #if UNITY_EDITOR || UNITY_STANDALONE
-        motion.x = Input.GetAxis("Horizontal");
+        motion.x = Input.GetAxisRaw("Horizontal");
         motion.y = 0f;
-        motion.z = Input.GetAxis("Vertical");
+        motion.z = Input.GetAxisRaw("Vertical");
 #endif
-
 #if UNITY_IOS
 #endif
         return motion.normalized;
     }
-    public static Vector3 usertargetCoordinateWorldSpace(LayerMask mask){
+
+    public static Vector3 targetCoordinateWorldSpace(LayerMask mask)
+    {
 
         return new Vector3();
+    }
+
+    public static bool isShooting()
+    {
+        bool fire = false;
+#if UNITY_EDITOR || UNITY_STANDALONE
+        if (Input.GetAxis("Fire1").Equals(true) || Input.GetAxis("Fire1") > 0.5f){
+            fire = true;
+        }
+#endif
+#if UNITY_IOS
+#endif
+        return fire;
     }
 }
