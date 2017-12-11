5c0b9777eca435f6437f2d16eed118c73698ec73
Wenzel Massag <mail@wmassag.de>
2017-12-11 22:42:36 +0100

    namespace, acceleration remapping to change sensitivity

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,92 +1,111 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class Health : MonoBehaviour {
+namespace theHeist
+{
+    public class Health : MonoBehaviour
+    {
 
-    /*
-     * Class keeps track of game objects health and shield-health
-     * This class does not manage collisions etc. GameObjects
-     * have to communicate health and shield changes
-    */
-
-    public bool regenerateWhileFightingWithShield = true;
-    public bool regenerateWhileFightingNoShield = false;
-    public float autoRegenerateDelayInSeconds = 0.5f;
-    public Transform destroyEffect; //optional
-    private float healthStatus = 1f; //ranges from 0...1 = percentage
-
-    //Health portion of class
-    public float getHealth(){
-        return healthStatus;
-    }
-    private IEnumerator selfDestructAtEndOfFrame(){
         /*
-         * Delayes objects destruction to allow frame to calculate collisions etc. corretly
+         * Class keeps track of game objects health and shield-health
+         * This class does not manage collisions etc. GameObjects
+         * have to communicate health and shield changes
         */
-        yield return new WaitForEndOfFrame();
-        Destroy(this.gameObject);
-    }
-    public float changeHealth(float difference, bool autoDestroy = true){
-        /*
-         * Public method to be called when GameObject's health is impacted.
-         * Calculates new health and schedules object to be destroyed
-         * returns health
-        */
-        healthStatus += difference;
-        healthStatus = Mathf.Clamp(shieldStatus, 0f, 1f);
-        if(autoDestroy && destroyEffect != null){
-            Instantiate(destroyEffect, this.transform.position, this.transform.rotation);
-            StartCoroutine(selfDestructAtEndOfFrame());
-        }
-        return getHealth();
-    }
 
-    //Shield portion of class
-    public bool hasShield = false;
-    public Transform shieldTransform; //optional
-    private float shieldStatus = 1f;
-    private Transform shieldInstance;
+        public bool regenerateWhileFightingWithShield = true;
+        public bool regenerateWhileFightingNoShield = false;
+        public float autoRegenerateDelayInSeconds = 0.5f;
+        public Transform destroyEffect; //optional
+        private float healthStatus = 1f; //ranges from 0...1 = percentage
 
-    public float getShieldStatus(){
-        return (hasShield) ? shieldStatus : 0f;
-    }
-    private void disableShield(){
-        hasShield = false;
-        if(shieldInstance != null){
-            Destroy(shieldInstance.gameObject);
-            shieldInstance = null;
+        //Health portion of class
+        public float getHealth()
+        {
+            return healthStatus;
         }
-    }
-    private void enableShield(){
-        hasShield = true;
-        shieldStatus = 1f; //make sure shield is charged
-        if(shieldInstance == null){
-            shieldInstance = Instantiate(shieldTransform, this.transform.position, this.transform.rotation, this.transform);
+        private IEnumerator selfDestructAtEndOfFrame()
+        {
+            /*
+             * Delayes objects destruction to allow frame to calculate collisions etc. corretly
+            */
+            yield return new WaitForEndOfFrame();
+            Destroy(this.gameObject);
         }
-    }
-    public float changeShieldStatus(bool b){
-        /*
-         * Public method to be called to enable or disable shield
-         * returns shield-status
-        */
-        if(b){
-            enableShield();
-        }else{
-            disableShield();
+        public float changeHealth(float difference, bool autoDestroy = true)
+        {
+            /*
+             * Public method to be called when GameObject's health is impacted.
+             * Calculates new health and schedules object to be destroyed
+             * returns health
+            */
+            healthStatus += difference;
+            healthStatus = Mathf.Clamp(shieldStatus, 0f, 1f);
+            if (autoDestroy && destroyEffect != null)
+            {
+                Instantiate(destroyEffect, this.transform.position, this.transform.rotation);
+                StartCoroutine(selfDestructAtEndOfFrame());
+            }
+            return getHealth();
         }
-        return getShieldStatus();
-    }
-    public float changeShieldStatus(float difference){
-        /*
-         * Public method to be called when GameObject's shield-health is impacted.
-         * Calculates new shield-health and disables shield if shield-health too low
-         * returns shield-status
-        */
-        shieldStatus += difference;
-        shieldStatus = Mathf.Clamp(shieldStatus, 0f, 1f);
-        if (shieldStatus.Equals(0f)){
-            disableShield();
+
+        //Shield portion of class
+        public bool hasShield = false;
+        public Transform shieldTransform; //optional
+        private float shieldStatus = 1f;
+        private Transform shieldInstance;
+
+        public float getShieldStatus()
+        {
+            return (hasShield) ? shieldStatus : 0f;
         }
-        return getShieldStatus();
+        private void disableShield()
+        {
+            hasShield = false;
+            if (shieldInstance != null)
+            {
+                Destroy(shieldInstance.gameObject);
+                shieldInstance = null;
+            }
+        }
+        private void enableShield()
+        {
+            hasShield = true;
+            shieldStatus = 1f; //make sure shield is charged
+            if (shieldInstance == null)
+            {
+                shieldInstance = Instantiate(shieldTransform, this.transform.position, this.transform.rotation, this.transform);
+            }
+        }
+        public float changeShieldStatus(bool b)
+        {
+            /*
+             * Public method to be called to enable or disable shield
+             * returns shield-status
+            */
+            if (b)
+            {
+                enableShield();
+            }
+            else
+            {
+                disableShield();
+            }
+            return getShieldStatus();
+        }
+        public float changeShieldStatus(float difference)
+        {
+            /*
+             * Public method to be called when GameObject's shield-health is impacted.
+             * Calculates new shield-health and disables shield if shield-health too low
+             * returns shield-status
+            */
+            shieldStatus += difference;
+            shieldStatus = Mathf.Clamp(shieldStatus, 0f, 1f);
+            if (shieldStatus.Equals(0f))
+            {
+                disableShield();
+            }
+            return getShieldStatus();
+        }
     }
 }
--- Assets/Scripts/Player/PlayerBehaviour.cs
+++ Assets/Scripts/Player/PlayerBehaviour.cs
@@ -2,63 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBehaviour : MonoBehaviour {
-
-    public float moveSpeed = 1;
-    private float backupInitialDragValue;
-    private Rigidbody me;
-    public Transform UserInputAbstractionEmpty;
-    private UserInputAbstraction abstractInputs;
-
-    void Start(){
-        backupInitialDragValue = GetComponent<Rigidbody>().drag;
-        me = GetComponent<Rigidbody>();
-        abstractInputs = UserInputAbstractionEmpty.GetComponent<UserInputAbstraction>();
-    }
-
-    private void setDragDefault(){
-        me.drag = backupInitialDragValue;
-    }
-
-    public void receiveForce(Vector3 force3D, float dragDifference = 0f){
-        /*
-         * Receives external forces and drag
-         * Example: Player runs into force field
-         * Example: Player runs into mud
-        */
-        force3D.y = 0f;
-        if (!force3D.magnitude.Equals(0f)){ //Only apply if relevant
-            me.AddForce(force3D, ForceMode.Force);
-        }
-        if (!dragDifference.Equals(0f)){ //Only apply if relevant
-            me.drag = backupInitialDragValue + dragDifference;
-        }
-    }
-
-    private void move(){
-        /*
-         * add motion to rigidbody using deltaTime and move Speed
-         * add motino as force, not at acceleration to avoid infinite speeds
-        */
-        me.AddForce(abstractInputs.motionVector() * moveSpeed * Time.deltaTime, ForceMode.Force);
-    }
-
-    private void shoot(){
-        if(UserInputAbstraction.isShooting()){
-            //GetComponent -> Weapon -> isFiring=true
-        }else{
-            //GetComponent -> Weapon -> isFiring=false
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
+namespace theHeist
+{
+    public class PlayerBehaviour : MonoBehaviour
     {
-        shoot(); //First shoot
-        move(); //Then Move
-        setDragDefault(); //Then reset drag, incase it was changed by environment
-                          /*
-                           * Drag reset must be done last, in order for external drag sources to have an impact on move()
-                          */
+
+        public float moveSpeed = 1;
+        private float backupInitialDragValue;
+        private Rigidbody me;
+        public Transform UserInputAbstractionEmpty;
+        private UserInputAbstraction abstractInputs;
+
+        void Start()
+        {
+            backupInitialDragValue = GetComponent<Rigidbody>().drag;
+            me = GetComponent<Rigidbody>();
+            abstractInputs = UserInputAbstractionEmpty.GetComponent<UserInputAbstraction>();
+        }
+
+        private void setDragDefault()
+        {
+            me.drag = backupInitialDragValue;
+        }
+
+        public void receiveForce(Vector3 force3D, float dragDifference = 0f)
+        {
+            /*
+             * Receives external forces and drag
+             * Example: Player runs into force field
+             * Example: Player runs into mud
+            */
+            force3D.y = 0f;
+            if (!force3D.magnitude.Equals(0f))
+            { //Only apply if relevant
+                me.AddForce(force3D, ForceMode.Force);
+            }
+            if (!dragDifference.Equals(0f))
+            { //Only apply if relevant
+                me.drag = backupInitialDragValue + dragDifference;
+            }
+        }
+
+        private void move()
+        {
+            /*
+             * add motion to rigidbody using deltaTime and move Speed
+             * add motino as force, not at acceleration to avoid infinite speeds
+            */
+            me.AddForce(abstractInputs.motionVector() * moveSpeed * Time.deltaTime, ForceMode.Force);
+        }
+
+        private void shoot()
+        {
+            if (UserInputAbstraction.isShooting())
+            {
+                //GetComponent -> Weapon -> isFiring=true
+            }
+            else
+            {
+                //GetComponent -> Weapon -> isFiring=false
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            shoot(); //First shoot
+            move(); //Then Move
+            setDragDefault(); //Then reset drag, incase it was changed by environment
+                              /*
+                               * Drag reset must be done last, in order for external drag sources to have an impact on move()
+                              */
+        }
     }
 }
